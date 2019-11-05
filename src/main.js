86d646799fc85ef41be2848ec188731c6b7f7728
IBOXPAY\zhaomeng <zhaomeng@iboxpay.com>
2019-11-05 20:05:53 +0800

    安装router+vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
+Vue.use(VueRouter).use(Vuex)
 Vue.config.productionTip = false
 
 new Vue({
